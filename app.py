5ce4a74db7157932b794a939a679b24c37da35ad
Abdul Rahman <abdulrahman75157@gmail.com>
2025-01-15 20:12:37 +0530

    feat!: implemented api for version

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-'''Main'''
-
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+from flask import Flask
 from version import __version__
 
-print(__version__)
+app = Flask(__name__)
+
+@app.route("/version")
+def print_version():
+    return __version__
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
