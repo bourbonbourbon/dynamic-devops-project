919b408d138e8715bb34501d647825c762a9a249
Abdul Rahman <abdulrahman75157@gmail.com>
2025-01-18 13:17:44 +0530

    feat: created /temperature api

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-function-docstring
+import datetime
 from flask import Flask
 from version import __version__
+from services.opensensemap import OpenSenseMap
 
 app = Flask(__name__)
+
 
 @app.route("/version")
 def print_version():
     return __version__
 
+
+@app.route("/temperature")
+def temp():
+    date = (
+        datetime.datetime.now()
+        .replace(
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0,
+        )
+        .isoformat()
+        + "Z"
+    )
+    api = OpenSenseMap(base_url="https://api.opensensemap.org")
+    data = api.get_all_temperatures(params=date)
+    return data
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")
---
+++ services/opensensemap.py
@@ -0,0 +1,60 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+import json
+import requests
+import pandas
+
+
+class OpenSenseMap:
+    def __init__(self, base_url, headers=None):
+        self.base_url = base_url
+        self.headers = headers
+
+    def get_all_temperatures(self, method="GET", params=None, data=None):
+        url = f"{self.base_url}/boxes?date={params}&phenomenon=temperature&format=json"
+        try:
+            response = requests.request(
+                method, url, headers=self.headers, params=params, data=data, timeout=15
+            )
+        except requests.exceptions.Timeout:
+            return "Query timed out."
+
+        if response.status_code == 200:
+
+            temperature_info = []
+
+            data = response.json()
+
+            for entry in data:
+                sensors = entry.get("sensors", [])
+
+                for sensor in sensors:
+                    if sensor.get("icon") == "osem-thermometer":
+
+                        if sensor.get("lastMeasurement", "") != "" and sensor.get("unit", "") == "°C":
+                            measurement_date = pandas.to_datetime(
+                                sensor.get("lastMeasurement", {}).get("createdAt", ""),
+                                utc=True,
+                            )
+                            current_date = pandas.to_datetime(params, utc=True)
+
+                            date_diff_in_hours = (
+                                measurement_date - current_date
+                            ).total_seconds() // 3600
+
+                            temperature_value = sensor.get("lastMeasurement", {}).get(
+                                "value", ""
+                            )
+
+                            if (
+                                measurement_date != ""
+                                and date_diff_in_hours < 24
+                                and temperature_value != ""
+                            ):
+                                temperature_info.append(float(temperature_value))
+
+            return str((sum(temperature_info) / len(temperature_info)))
+
+        else:
+            return f"Error: {response.status_code}, {response.text}"
