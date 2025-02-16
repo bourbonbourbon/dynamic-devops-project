f5014a1ede1a01109c988dc5bf53a832d4e8ddf4
Abdul Rahman <abdulrahman75157@gmail.com>
2025-02-16 17:13:08 +0530

    refactor: opensensemap.py increased timeout to 120s

--- services/opensensemap.py
+++ services/opensensemap.py
@@ -9,34 +9,33 @@ class OpenSenseMap:
     def __init__(self, base_url, headers=None):
         self.base_url = base_url
         self.headers = headers
-        self.current_date = ""
 
     def get_avg_temperature(self, method="GET", params=None, data=None):
         url = f"{self.base_url}/boxes?date={params}&phenomenon=temperature&format=json"
         response = requests.request(
-            method, url, headers=self.headers, params=params, data=data, timeout=60
+            method, url, headers=self.headers, params=params, data=data, timeout=120
         )
-        self.current_date = params
 
         if response.status_code == 200:
-            return self._process_temperature_data(response.json()), 200
+            return self._process_temperature_data(response.json(), params), 200
 
         return {"err_msg": f"Error: {response.status_code}, {response.text}"}, 503
 
-    def _process_temperature_data(self, data):
+    def _process_temperature_data(self, data, date):
         temperature_info = []
 
         for entry in data:
             sensors = entry.get("sensors", [])
-            temperature_info.extend(self._get_sensor_temperatures(sensors))
+            temperature_info.extend(self._get_sensor_temperatures(sensors, date))
 
         if temperature_info:
             return {"avg_temp": sum(temperature_info) / len(temperature_info)}
         return {"avg_temp": 0}
 
-    def _get_sensor_temperatures(self, sensors):
+    def _get_sensor_temperatures(self, sensors, date):
         temperatures = []
-        current_date = datetime.datetime.fromisoformat(self.current_date)
+        current_date = datetime.datetime.fromisoformat(date)
+        print(current_date, type(current_date))
 
         for sensor in sensors:
             if sensor.get("icon") == "osem-thermometer":
@@ -50,7 +49,7 @@ class OpenSenseMap:
                     ).total_seconds() // 3600
                     temperature_value = last_measurement.get("value", "")
 
-                    if date_diff_in_hours < 24 and temperature_value:
+                    if date_diff_in_hours < 24 and temperature_value != "":
                         temperatures.append(float(temperature_value))
 
         return temperatures
