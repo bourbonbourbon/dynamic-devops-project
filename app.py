721e706fc4b65cdf2ff503ff52ce4bbbd10dda58
Abdul Rahman <abdulrahman75157@gmail.com>
2025-02-25 11:06:26 +0530

    feature: added prometheus_client

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-function-docstring
 import datetime
+from prometheus_client import Counter, generate_latest
 from flask import Flask, jsonify
 from version import __version__
 from services.opensensemap import OpenSenseMap
 
+total_version_requests = Counter("version_requests", "Total Number of version requests")
+total_temp_requests = Counter("temp_requests", "Total Number of temperature requests")
+
 app = Flask(__name__)
 
+@app.route("/metrics")
+def metrics():
+    return generate_latest()
 
 @app.route("/version")
 def print_version():
+    total_version_requests.inc()
     return jsonify({"version": __version__}), 200
 
 
@@ -28,6 +36,7 @@ def temperature():
     )
     api = OpenSenseMap(base_url="https://api.opensensemap.org")
     data, return_code = api.get_avg_temperature(params=date)
+    total_temp_requests.inc()
     return jsonify(data), return_code
 
 
